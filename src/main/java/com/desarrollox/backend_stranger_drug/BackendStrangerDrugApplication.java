19b1510db1983ffe30bae01be0b3fa669b39a85a
payan3521 <pablobedoya3521@gmail.com>
2025-11-22 19:20:58 -0500

    initial commit

---
+++ src/main/java/com/desarrollox/backend_stranger_drug/BackendStrangerDrugApplication.java
@@ -0,0 +1,13 @@
+package com.desarrollox.backend_stranger_drug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStrangerDrugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendStrangerDrugApplication.class, args);
+	}
+
+}
